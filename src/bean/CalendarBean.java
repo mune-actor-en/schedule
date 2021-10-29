1e50b12d5553dbd1635c569823a3e1aa8fc4d501
m-hiroiwa <masamune.hiroiwa@fusionia.net>
2021-10-30 01:28:07 +0900

    カレンダーモデルのクラスを新規追加。カレンダーの処理を追記。

---
+++ src/bean/CalendarBean.java
@@ -0,0 +1,37 @@
+package bean;
+import java.io.Serializable;
+
+public class CalendarBean implements Serializable {
+	// カレンダーの年
+	private int year;
+	// カレンダーの月
+	private int month;
+	// カレンダーのデータを保持する配列
+	private String[][] data;
+
+	// セッター・ゲッターを定義
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public int getYwar() {
+		return year;
+	}
+
+	public void setMonth(int month) {
+		this.month = month;
+	}
+
+	public int getMonth() {
+		return month;
+	}
+
+	public void setData(String [][] data) {
+		this.data = data;
+	}
+
+	public String[][] getData(){
+		return data;
+	}
+
+}
--- src/bean/MeetingCalendar.java
+++ src/bean/MeetingCalendar.java
@@ -3,6 +3,25 @@ package bean;
 import java.util.Calendar;
 
 public class MeetingCalendar {
-	Calendar cal = Calendar.getInstance();
+	// カレンダーインスタンスを生成するメソッド
+	public CalendarBean createCalendar(int... args) {
+		// カレンダーモデルをインスタンス生成
+		CalendarBean cb = new CalendarBean();
+		// 現在の日時でカレンダーを生成
+		Calendar cal = Calendar.getInstance();
+		if(args.length == 2) {
+			// 最初の引数で年を指定
+			cal.set(Calendar.YEAR, args[0]);
+			// 次の引数で月を指定
+			cal.set(Calendar.MONTH, args[1]-1);
+		}
+
+		// カレンダーに年を設定
+		cb.setYear(cal.get(Calendar.YEAR));
+		// カレンダーに月を設定
+		cb.setMonth(cal.get(Calendar.MONTH) + 1);
+
+	}
+
 
 }
