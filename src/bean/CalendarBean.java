c22c0b51a4926e116e10013da6131e96be34e796
m-hiroiwa <masamune.hiroiwa@fusionia.net>
2021-12-06 23:32:43 +0900

    予約状況のデータ取得クエリを作成

--- src/bean/CalendarBean.java
+++ src/bean/CalendarBean.java
@@ -8,6 +8,19 @@ public class CalendarBean implements Serializable {
 	private int month;
 	// カレンダーのデータを保持する配列
 	private String[][] data;
+	
+	// 予約済みの会議室名
+	private String room_name;
+	
+	// 予約しているユーザー名
+	private String user_name;
+	
+	// 利用開始時間
+	private String use_start_time;
+	
+	// 利用終了時間
+	private String use_end_time;
+	
 
 	// セッター・ゲッターを定義
 	public void setYear(int year) {
@@ -32,6 +45,38 @@ public class CalendarBean implements Serializable {
 
 	public String[][] getData(){
 		return data;
+	}
+
+	public void setRoomName(String room_name) {
+		this.room_name = room_name;
+	}
+	
+	public String getRoomName() {
+		return room_name;
+	}
+	
+	public void setUserName(String user_name) {
+		this.user_name = user_name;
+	}
+	
+	public String getUserName() {
+		return user_name;
+	}
+	
+	public void setUseStartTime(String use_start_time) {
+		this.use_start_time = use_start_time;
+	}
+	
+	public String getUseStartTime() {
+		return use_start_time;
+	}
+	
+	public void setUseEndTime(String use_end_time) {
+		this.use_end_time = use_end_time;
+	}
+	
+	public String getUseEndTime() {
+		return use_end_time;
 	}
 
 }
--- src/service/ScheduleService.java
+++ src/service/ScheduleService.java
@@ -7,55 +7,88 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-import bean.ReservationStatusBean;
+import bean.CalendarBean;
 
 public class ScheduleService {
 
 	// ドライバーのクラス名
-    private static final String POSTGRES_DRIVER = "org.postgresql.Driver";
-    // JDMC接続先情報
-    private static final String JDBC_CONNECTION = "jdbc:postgresql://localhost:5433/schedule";
-    // ユーザー
-    private static final String USER = "postgres";
-    // パスワード
-    private static final String PASS = "postgres";
-    
-    // 予約状況クラスのインスタンスを生成
-    ReservationStatusBean reservationStatus = new ReservationStatusBean();
+	private static final String POSTGRES_DRIVER = "org.postgresql.Driver";
+	// JDMC接続先情報
+	private static final String JDBC_CONNECTION = "jdbc:postgresql://localhost:5433/schedule";
+	// ユーザー
+	private static final String USER = "postgres";
+	// パスワード
+	private static final String PASS = "postgres";
+	// タイムフォーマット
+	private static final String TIME_FORMAT = "HH:mm";
+	// 予約状況を取得するSQL
+	private static final String SELECT_SQL = "SELECT mr.room_name, rs.user_name, rs.use_start_time, rs.use_end_time FROM meeting_rooms mr INNER JOIN reservation_status rs ON mr.room_id = rs.room_id;";
 
-    public static void main(String[] args) {
+	// 予約状況クラスのインスタンスを生成
+	// ReservationStatusBean reservationStatus = new ReservationStatusBean();
+	CalendarBean calendarbean = null;
+	
+	// public String roomName = calendarbean.getRoomName();
 
-        Connection connection = null;
-        Statement statement = null;
-        ResultSet resultSet = null;
-        PreparedStatement preparedStatement = null;
+	public static void main(String[] args) {
 
-        try {
-        	// データベースに接続
-            Class.forName(POSTGRES_DRIVER);
-            // 接続先の情報
-            connection = DriverManager.getConnection(JDBC_CONNECTION, USER, PASS);
-            statement = connection.createStatement();
+		Connection connection = null;
+		Statement statement = null;
+		ResultSet resultSet = null;
+		PreparedStatement preparedStatement = null;
 
-        // forName()で例外発生
-        } catch(ClassNotFoundException e) {
-        	e.printStackTrace();
+		try {
+			// データベースに接続
+			Class.forName(POSTGRES_DRIVER);
+			// 接続先の情報
+			connection = DriverManager.getConnection(JDBC_CONNECTION, USER, PASS);
+			statement = connection.createStatement();
 
-        // getConnection()で例外発生
-        } catch(SQLException e) {
-        	e.printStackTrace();
+			// SimpleDateFormat sdFormat = new SimpleDateFormat(TIME_FORMAT);
+			// String use_start_time = sdFormat.format(calendarbean.getUseStartTime());
+			
+			preparedStatement = connection.prepareStatement(SELECT_SQL);
+			// preparedStatement.setString(1, roomName);
+			
+			resultSet = preparedStatement.executeQuery();
+			
+			while(resultSet.next()) {
+				String tmpRoomName = resultSet.getString("room_name");
+				String tmpUserName = resultSet.getString("user_name");
+				String tmpUseStartTime = resultSet.getString("user_start_time");
+				String tmpUserEndTime = resultSet.getString("use_end_time");
+				
+				// calendarBean = new CalendarBean();
+			}
+			
+			// forName()で例外発生
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
 
-        } finally {
-        	try {
-        		if (connection != null) {
-        			// データベースを切断
-        			connection.close();
-        		}
+			// getConnection()で例外発生
+		} catch (SQLException e) {
+			e.printStackTrace();
 
-        	} catch(SQLException e) {
-        		e.printStackTrace();
-        	}
-        }
-    }
+		} finally {
+			try {
+				
+				if(resultSet != null) {
+					resultSet.close();
+				}
+				
+				if(statement != null) {
+					statement.close();
+				}
+				
+				if (connection != null) {
+					// データベースを切断
+					connection.close();
+				}
+
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 
 }
